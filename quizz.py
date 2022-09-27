9b0f34425b1bc7de573316d87556f87325c4ebb8
GoranMarjanovic90 <goranmarjanovic1990@gmail.com>
2022-09-27 16:43:30 +0200

    change

--- quizz.py
+++ quizz.py
@@ -5,9 +5,9 @@ def fråga(info_text, text1, text2, text3, text4, rätt_svar):
     print("1", text1)
     print("2", text2)
     print("3", text3)
-    print("3", text4)
+    print("4", text4)
     print("Skriv rätt svar:")
-    svar = int(input())
+    svar = int(input(">"))
     if(rätt_svar == svar):
         print("rätt svar")
     else:
@@ -20,6 +20,6 @@ fråga("Vad betyder 'unpythonic' i Python community?", "Kod med många fel", "Ko
 fråga("Vilket av dessa program är INTE skrivet i Python?", "Reddit", "Pinterest", "Adobe Photoshop","Adobe illstrator", 3)
 fråga("Uppträdde första gången?", "1990", "1991", "1993","1994", 2 )
 fråga("Vilken Extraherar tillägg från filnamn i Python?", "Py", "Txt", "Psd", "png", 1)
-fråga("Vad betyder sträng i Python", "Heltal", "Texten", "flyttal", 2)
+fråga("Vad betyder sträng i Python", "Heltal", "Texten", "flyttal", "True", 2)
 fråga("Vad betyder 'unpythonic' i Python community?", "Kod med många fel", "Kod som är svår att förstå eller läsa", "Kod som skriven på ett annat programmeringsspråk", "Svar 1 och 2", 2)
 fråga("varfår kallas språket så?", "Namnet kommer från en reptil", "Namnet på språket kommer från BBC komediserie Monty Pythons Flying Circus", "Efternamnet på skaparen är Python", "vet ej", 2)
