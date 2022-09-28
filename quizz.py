b2b6d10aa509467e355065415731d421108e7405
Walter Ahlin <waltersten.ahlin@gmail.com>
2022-09-28 17:01:20 +0200

    UpdateNew

--- quizz.py
+++ quizz.py
@@ -28,5 +28,6 @@ fråga("Vilket av dessa program är INTE skrivet i Python? \n ", "Reddit", "Pint
 fråga("När skapades Python? \n ", "1990", "1991", "1993","1994", 2 )
 fråga("Vilken Extraherar tillägg från filnamn i Python? \n ", "Py", "Txt", "Psd", "png", 1)
 fråga("Vad betyder sträng i Python? \n ", "Heltal", "Texten", "flyttal", "True", 2)
+fråga("Vad är Python för typ av språk? \n ", "Lågnivå språk", "Mellanvivå språk", "Högnivå språk", "Inget av de föregående alternativen", 3)
 
 print(" Tack för att du spelade! Ditt resultat är: ", score, "rätta svar!")
