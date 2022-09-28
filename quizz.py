769bd0b7abb43fe7bbc1977b1317f7b3b5d60f67
Walter Ahlin <waltersten.ahlin@gmail.com>
2022-09-28 14:21:15 +0200

    Revert "Update quizz.py"
    This reverts commit 6e724ad353c2e77cc9674fce6e02dee65901dc0a.

--- quizz.py
+++ quizz.py
@@ -1,17 +1,24 @@
-print("välkomen till Python frågor")
 
-def fråga(info_text, text1, text2, text3, text4, rätt_svar):
+print("Vällkomen till Python Quiz! \n ")
+alla_frågor = 9
+score = 0
+
+def fråga(info_text, text1, text2, text3, text4, rätt_svar ):
+    global score
     print(info_text)
     print("1", text1)
     print("2", text2)
     print("3", text3)
     print("4", text4)
-    print("Skriv rätt svar:")
+    print("Skriv rätt svar: \n ")
     svar = int(input(">"))
     if(rätt_svar == svar):
-        print("rätt svar")
+        print("Rätt svar \n ")
+        score += 1
+        
     else:
         print("Fel")
+       
 
 fråga("Varför kallas språket så? \n ", "Namnet kommer från en reptil", "Namnet på språket kommer från BBC komediserie Monty Python's Flying Circus", "Efternamnet på skaparen är Python","Svar 2 och 3", 3)
 fråga("På vilken plats kom Python i TIOBE index(språkets popularitet) 2021? \n ", "Tredje plats", "Andra plats", "Första plats","Fjarde plats", 3)
