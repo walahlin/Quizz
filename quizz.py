71e931d23efde4a8f4430cae93269c929060163e
jekolsh <ek.olshannikova@gmail.com>
2022-09-30 09:43:22 +0200

    Ändrade svar i fråga 5

--- quizz.py
+++ quizz.py
@@ -22,7 +22,7 @@ fråga("Varför kallas språket så? \n ", "Namnet kommer från en reptil", "Nam
 fråga("På vilken plats kom Python i TIOBE index(språkets popularitet) 2021? \n ", "Tredje plats", "Andra plats", "Första plats","Fjarde plats", 3)
 fråga("Pythons kärnfilosofi sammanfattas i dokumentet The Zen of Python PEP 203 Vilket av dessa påståenden gäller inte Pythons filosofi? \n ", "Läsbarheten räknas", "Vackert är bättre än fult", "Det bästa är fiende till det goda","Inga av de", 3)
 fråga("Vad betyder 'unpythonic' i Python community? \n ", "Kod med många fel", "Kod som är svår att förstå eller läsa", "Kod som skriven på ett annat programmeringsspråk","Svar 1 + 2", 2)
-fråga("Vilket av dessa program är INTE skrivet i Python? \n ", "Reddit", "Pinterest", "Adobe Photoshop","Adobe illstrator", 3)
+fråga("Vilket av dessa program är INTE skrivet i Python? \n ", "Reddit", "Pinterest", "Adobe Photoshop","Sims 4", 3)
 fråga("När skapades Python? \n ", "1990", "1991", "1993","1994", 2 )
 fråga("Vilken Extraherar tillägg från filnamn i Python? \n ", "Py", "Txt", "Psd", "png", 1)
 fråga("Vad betyder sträng i Python? \n ", "Heltal", "Texten", "flyttal", "True", 2)
